40a68ae664e047922dff13f947b5ab91ba8234c5
Viniciusulpicio <viniciusulpicio@gmail.com>
2024-10-16 17:24:24 -0300

    :ok_hand: OK

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,7 @@ class _MyWidgetState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder(
-      child: Text("test tasdasdsop"),
+      child: Text("home"),
     );
   }
 }
