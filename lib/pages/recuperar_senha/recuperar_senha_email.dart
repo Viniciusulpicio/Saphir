23285652c2589708cda377f1e4aad27aceb28096
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2024-10-22 17:24:49 -0300

    commit iniciando pagina

--- lib/pages/recuperar_senha/recuperar_senha_email.dart
+++ lib/pages/recuperar_senha/recuperar_senha_email.dart
@@ -10,8 +10,27 @@ class RecuperarSenhaEmail extends StatefulWidget {
 class _MyWidgetState extends State<RecuperarSenhaEmail> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text("jhuan incrivel"),
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 30, 30, 30),
+      body: Center(
+          child: SingleChildScrollView(
+              child: Form(
+                  child: SingleChildScrollView(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+            Text(
+              "saphir",
+              style: TextStyle(
+                color: Color.fromARGB(255, 23, 44, 228),
+                fontSize: 60,
+                fontFamily: 'DaysOne',
+              ),
+            ),
+            Text("Digite o email de recuperação:", style: TextStyle(color: Colors.white)),
+            Padding(padding:  const EdgeInsets.symmetric(horizontal: 30.0),
+            child:  ,)
+          ]))))),
     );
   }
 }
