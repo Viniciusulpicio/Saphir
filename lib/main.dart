0dfe0f6895c409a6b6f5f5daec81687c79f7404b
Jhuan-Medeiros <jhuan.mcordeiro@gmail.com>
2024-11-01 10:20:35 -0300

    ✨ página de editar perfil

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:saphir/pages/acao.dart';
 import 'package:saphir/pages/casual.dart';
+import 'package:saphir/pages/editarPerfil.dart';
 import 'package:saphir/pages/jogos/bloons_TD.dart';
 import 'package:saphir/pages/jogos/elden_ring.dart';
 import 'package:saphir/pages/jogos/elder_scrolls.dart';
@@ -35,7 +36,6 @@ import 'package:saphir/pages/recuperar_senha/nova_senha.dart';
 import 'package:saphir/pages/recuperar_senha/recuperar_senha_codigo.dart';
 import 'package:saphir/pages/recuperar_senha/recuperar_senha_email.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -53,46 +53,44 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: MyColors.azulEscuro),
         useMaterial3: true,
       ),
-
-      initialRoute: "/salvos",
+      initialRoute: "/editarPerfil",
       routes: {
         // '/' : (context) => const Splash(),
-        '/login' : (context) => const Login(),
-        '/cadastro' : (context) => const Cadastro(),
-        '/home' : (context) => const Home(),
-        '/configuracao' : (context) => const Configuracao(),
-        '/conta' : (context) => const Conta(),
-        '/novaSenha' : (context) => const NovaSenha(),
-        '/recuperarCodigo' : (context) => const RecuperarSenhaCodigo(),
-        '/recuperarEmail' : (context) => const RecuperarSenhaEmail(),
-        '/salvos' : (context) => const Salvos(),
-        '/acao' : (context) => const Acao(),
-        '/casual' : (context) => const Casual(),
-        '/luta' : (context) => const Luta(),
-        '/rpg' : (context) => const Rpg(),
-        '/simulacao' : (context) => const Simulacao(),
-        '/terror' : (context) => const Terror(),
-
+        '/login': (context) => const Login(),
+        '/cadastro': (context) => const Cadastro(),
+        '/home': (context) => const Home(),
+        '/configuracao': (context) => const Configuracao(),
+        '/conta': (context) => const Conta(),
+        '/novaSenha': (context) => const NovaSenha(),
+        '/recuperarCodigo': (context) => const RecuperarSenhaCodigo(),
+        '/recuperarEmail': (context) => const RecuperarSenhaEmail(),
+        '/salvos': (context) => const Salvos(),
+        '/acao': (context) => const Acao(),
+        '/casual': (context) => const Casual(),
+        '/luta': (context) => const Luta(),
+        '/rpg': (context) => const Rpg(),
+        '/simulacao': (context) => const Simulacao(),
+        '/terror': (context) => const Terror(),
+        '/editarPerfil': (context) => const Editarperfil(),
         // jogos
-        '/lifeIs' : (context) =>  const LifeIsStrange(),
-        '/redDead' : (context) => const RedDead(),
-        '/starWars' : (context) => const StarWars(),
-        '/bloondTD' : (context) =>  const BloonsTd(),
-        '/eldenRing' : (context) => const EldenRing(),
-        '/elderScrolls' : (context) => const ElderScrolls(),       
-        '/legoCity' : (context) => const LegoCity(),        
-        '/legoHerois' : (context) => const LegoSuperHerois(),
-        '/legoBatman' : (context) => const Legobatman(),       
-        '/mineirinhoUltra' : (context) => const MineirinhoUltra(),
-        '/mortalKombat' : (context) => const MortalKombat(),
-        '/outlast' : (context) => const Outlast(),        
-        '/stardewValley' : (context) => const StardewValley(),
-        '/silentHill' : (context) => const SilentHill(),
-        '/theLastUs' : (context) => const TheLastUs(),       
-        '/theSims2' : (context) => const TheSims2(),
-        '/theSims4' : (context) => const TheSims4(),
-
+        '/lifeIs': (context) => const LifeIsStrange(),
+        '/redDead': (context) => const RedDead(),
+        '/starWars': (context) => const StarWars(),
+        '/bloondTD': (context) => const BloonsTd(),
+        '/eldenRing': (context) => const EldenRing(),
+        '/elderScrolls': (context) => const ElderScrolls(),
+        '/legoCity': (context) => const LegoCity(),
+        '/legoHerois': (context) => const LegoSuperHerois(),
+        '/legoBatman': (context) => const Legobatman(),
+        '/mineirinhoUltra': (context) => const MineirinhoUltra(),
+        '/mortalKombat': (context) => const MortalKombat(),
+        '/outlast': (context) => const Outlast(),
+        '/stardewValley': (context) => const StardewValley(),
+        '/silentHill': (context) => const SilentHill(),
+        '/theLastUs': (context) => const TheLastUs(),
+        '/theSims2': (context) => const TheSims2(),
+        '/theSims4': (context) => const TheSims4(),
       },
     );
   }
-} 
+}
